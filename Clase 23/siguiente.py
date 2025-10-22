33fadf5e3a8bd294e607a7368ff6baf9cd92a782
Omar Fazzito <ofazzito@yahoo.com.ar>
2025-10-22 17:29:34 -0300

    Clase 23

---
+++ Clase 23/siguiente.py
@@ -0,0 +1,12 @@
+# ESTO ES SOLO UN EJEMPLO CONCEPTUAL PARA EL SIGUIENTE PASO
+# from langchain.llms import ...
+# from langchain.chains import RetrievalQA
+
+# llm = ... # Cargaríamos un LLM
+# qa_chain = RetrievalQA.from_chain_type(
+#     llm=llm,
+#     chain_type="stuff",
+#     retriever=vectordb.as_retriever()
+# )
+# respuesta = qa_chain.run("¿Qué es LangChain?")
+# print(respuesta)
